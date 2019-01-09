2b9d33b19e50bfa09520fa163caab7cbce378266
nitayneeman <nitayneeman@users.noreply.github.com>
2019-01-09 16:11:30 +0200

    <Box/> - improved API docs

--- src/Box/Box.js
+++ src/Box/Box.js
@@ -16,7 +16,8 @@ const verticalAlignmentValues = {
   bottom: 'flex-end',
 };
 
-const formatValue = value => isFinite(value) ? value * spacingUnit : `${value}`;
+/** In case the value is a number, it's multiplied by the spacing unit. Otherwise, it's converted to a string */
+const formatSpacingValue = value => isFinite(value) ? value * spacingUnit : `${value}`;
 
 const Box = props => {
   const {
@@ -51,16 +52,16 @@ const Box = props => {
     alignItems: verticalAlignmentValues[verticalAlign],
 
     // Spacing
-    padding: formatValue(padding),
-    paddingTop: formatValue(paddingTop),
-    paddingRight: formatValue(paddingRight),
-    paddingBottom: formatValue(paddingBottom),
-    paddingLeft: formatValue(paddingLeft),
-    margin: formatValue(margin),
-    marginTop: formatValue(marginTop),
-    marginRight: formatValue(marginRight),
-    marginBottom: formatValue(marginBottom),
-    marginLeft: formatValue(marginLeft),
+    padding: formatSpacingValue(padding),
+    paddingTop: formatSpacingValue(paddingTop),
+    paddingRight: formatSpacingValue(paddingRight),
+    paddingBottom: formatSpacingValue(paddingBottom),
+    paddingLeft: formatSpacingValue(paddingLeft),
+    margin: formatSpacingValue(margin),
+    marginTop: formatSpacingValue(marginTop),
+    marginRight: formatSpacingValue(marginRight),
+    marginBottom: formatSpacingValue(marginBottom),
+    marginLeft: formatSpacingValue(marginLeft),
 
     // Sizing
     minWidth,
@@ -81,26 +82,48 @@ const Box = props => {
 Box.displayName = 'Box';
 
 Box.propTypes = {
+  /** Any element to be rendered inside */
   children: PropTypes.node.isRequired,
+  /** Defines if the box behaves as an inline element */
   inline: PropTypes.bool,
+  /** Defines how the children are aligned according to the X axis */
   align: PropTypes.string,
+  /** Defines how the children are aligned according to the Y axis */
   verticalAlign: PropTypes.string,
+  /** Sets padding on all sides */
   padding: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Sets padding on the top */
   paddingTop: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Sets padding on the right */
   paddingRight: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Sets padding on the bottom */
   paddingBottom: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Sets padding on the left */
   paddingLeft: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Sets margin on all sides */
   margin: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Sets margin on the top */
   marginTop: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Sets margin on the right */
   marginRight: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Sets margin on the bottom */
   marginBottom: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Sets margin on the left */
   marginLeft: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Sets the minimum width of the box */
   minWidth: PropTypes.string,
+  /** Sets the maximum width of the box */
   maxWidth: PropTypes.string,
+  /** Sets the width of the box */
   width: PropTypes.string,
+  /** Sets the minimum height of the box */
   minHeight: PropTypes.string,
+  /** Sets the maximum height of the box */
   maxHeight: PropTypes.string,
+  /** Sets the height of the box */
   height: PropTypes.string,
+  /** Applied as data-hook HTML attribute that can be used in the tests */
+  dataHook: PropTypes.string,
 };
 
 Box.defaultProps = {
